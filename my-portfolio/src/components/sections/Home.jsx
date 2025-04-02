61fc4f0fbc176d999edd1451e7f970b75265d0e8
Golam Rahman <80764905+IKIGA-I@users.noreply.github.com>
2025-04-02 19:07:54 +0600

    Resume Addition

--- my-portfolio/src/components/sections/Home.jsx
+++ my-portfolio/src/components/sections/Home.jsx
@@ -30,6 +30,15 @@ export const Home = () => {
                         hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover: bg-blue-500/10">
                         Contact Me
                     </a>
+                    
+                </div>
+                <div className="flex justify-center space-x-4 py-4">
+                    <a 
+                        href="https://drive.google.com/file/d/1dtBE6i1GLdIlpQYjCBmfDQKP_C-v5Ej4/view?usp=sharing" 
+                        className="bg-green-800 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 
+                        hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">
+                        Resume
+                    </a>
                 </div>
             </div>
             </RevealOnScroll>
