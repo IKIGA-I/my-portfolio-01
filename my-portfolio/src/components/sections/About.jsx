f37fa7c7053a6a89f2463e9c57cc93104bfb2f31
Golam Rahman <80764905+IKIGA-I@users.noreply.github.com>
2025-04-02 19:28:12 +0600

    Border Adjustment and Hover effect for About section

--- my-portfolio/src/components/sections/About.jsx
+++ my-portfolio/src/components/sections/About.jsx
@@ -25,7 +25,8 @@ export const About = () => {
             <div className="max-w-3xl mx-auto px-4">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"> About Me</h2>
             
-                <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+                <div className="rounded-xl p-8 border-white/10 border-2 hover:-translate-y-1 transition-all hover:border-blue-500/30 
+                                    hover:shadow-[0_2px_8px_rgba(59, 130,246,0.2)] transition">
                     <p className="text-gray-300 mb-6">
                         Hi, I’m <b>Golam Rahman</b>, an Electrical and Electronics Engineer with a growing passion for <b>embedded systems and Linux driver development</b>.<br /><br />  
                         I graduated from <b>Xiamen University Malaysia</b> and have worked with <b>ESP32, Arduino, and Verilog for digital design verification</b>.<br /><br />  
@@ -75,7 +76,8 @@ export const About = () => {
                 </div>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all hover:border-blue-500/30 
+                                    hover:shadown-[0_2px_8px_rgba(59, 130,246,0.2)] transition">
                         <h3 className="text-xl font-bold mb-4"> 📚 Education </h3>
                         <ul className="list-disc list-inside text-gray-300 space-y-2">
                             <li>
@@ -87,7 +89,8 @@ export const About = () => {
                         </ul>
                     </div>
 
-                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all hover:border-blue-500/30 
+                                    hover:shadown-[0_2px_8px_rgba(59, 130,246,0.2)] transition">
                         <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
                         <div className="space-y-4 text-gray-300">
                             <div>
